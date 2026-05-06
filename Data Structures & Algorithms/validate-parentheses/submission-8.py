bcd9e060394e08e7db1a57b804a07620d6207917
bamasene1 <83519340+bamasene1@users.noreply.github.com>
2026-05-06 18:10:20 -0500

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.py
@@ -0,0 +1,30 @@
+#U:trying to see if series of brackets are closed properly
+#P: make a dict that holds the opening:closing brackets for search. iterate thrught the string:
+# if the val is an opening bracket push to the stack, if its a closer, check that the val at the top
+# is the correspondinng opener
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        pMap = {"(":")","{":"}","[":"]"}
+
+        if len(s) == 1 or s[0] in pMap.values(): 
+            return False
+
+        for i in s:
+            if i in pMap.keys(): 
+                stack.append(i)
+                print(i)
+                print(stack)
+                pass
+            elif i in pMap.values():
+                if not stack:
+                    return False
+                comp = stack.pop()
+                print(comp)
+                if pMap[comp] != i:
+                    return False
+        if not stack:
+            return True
+        else:
+            return False
+            
